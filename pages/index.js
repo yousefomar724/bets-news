11482021fecd2e028b06977a37bf2c4011cd6c9b
Yousef Omar <yousefomar724@gmail.com>
2022-07-13 00:03:20 +0200

    test without allPosts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-import HeroPost from '@/components/home/hero-post'
-import HomePost from '@/components/home/home-content'
-import Layout from '@/components/layouts/layout'
+// import HeroPost from '@/components/home/hero-post'
+// import HomePost from '@/components/home/home-content'
+import dynamic from 'next/dynamic'
+const HomePost = dynamic(() => import('@/components/home/home-content'))
+const HeroPost = dynamic(() => import('@/components/home/hero-post'))
+const Layout = dynamic(() => import('@/components/layouts/layout'))
+const CategoryMatrix = dynamic(() =>
+  import('@/components/shared/categories-matrix')
+)
+const TagPostsGrid = dynamic(() => import('@/components/post/posts-by-tag-002'))
 import Meta from '@/components/meta/meta'
-import CategoryMatrix from '@/components/shared/categories-matrix'
-import TagPostsGrid from '@/components/post/posts-by-tag-002'
+// import Layout from '@/components/layouts/layout'
+// import CategoryMatrix from '@/components/shared/categories-matrix'
+// import TagPostsGrid from '@/components/post/posts-by-tag-002'
 import { getAllPostsForHome, getPostsByTag, getAllSettings } from '@/lib/api'
 import { getAllTags } from '@/lib/api'
 
@@ -17,16 +25,16 @@ export default function Index({
   customTagLink,
   tags,
 }) {
-  console.log(tags)
-  const heroPost = allPosts.slice(0)
-  const thenPosts = allPosts.slice(0)
-  const morePosts = allPosts.slice(0)
+  // console.log(tags)
+  // const heroPost = allPosts.slice(0)
+  // const thenPosts = allPosts.slice(0)
+  // const morePosts = allPosts.slice(0)
   return (
     <>
       <Meta settings={settings} />
       <Layout settings={settings}>
-        {heroPost && <HeroPost posts={heroPost} />}
-        {morePosts.length > 0 && (
+        {/* {heroPost && <HeroPost posts={heroPost} />} */}
+        {/* {morePosts.length > 0 && (
           <HomePost
             posts={morePosts}
             thenPosts={thenPosts}
@@ -34,7 +42,7 @@ export default function Index({
             all_posts_count={allPosts.lenght}
             tag_top={tagPostsTop}
           />
-        )}
+        )} */}
         {<CategoryMatrix settings={settings} />}
         {
           <TagPostsGrid
@@ -49,7 +57,7 @@ export default function Index({
 }
 
 export async function getStaticProps() {
-  const allPosts = (await getAllPostsForHome()) || []
+  // const allPosts = (await getAllPostsForHome()) || []
   const tagPostsTop = (await getPostsByTag('noticias-do-dia')) || []
   const tagPostsBrasil = (await getPostsByTag('brasil', 5)) || []
   const settings = await getAllSettings()
@@ -57,12 +65,12 @@ export async function getStaticProps() {
   const customTagTitle = 'Brasil'
   const customTagLink = '/tag/brasil'
 
-  const postMeta = allPosts.meta
+  // const postMeta = allPosts.meta
   return {
     props: {
-      allPosts,
+      // allPosts,
       tagPostsTop,
-      postMeta,
+      // postMeta,
       settings,
       tagPostsBrasil,
       customTagTitle,
